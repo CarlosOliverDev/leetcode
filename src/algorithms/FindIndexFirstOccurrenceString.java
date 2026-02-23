58dd3422e69ba60e6bd67e32e8549f6b3529a482
Carlos <carlos.oli.h23@gmail.com>
2026-02-23 19:30:45 -0300

    Exercício resolvido sem métodos da String.

--- src/algorithms/FindIndexFirstOccurrenceString.java
+++ src/algorithms/FindIndexFirstOccurrenceString.java
@@ -26,13 +26,41 @@ haystack and needle consist of only lowercase English characters.
 public class FindIndexFirstOccurrenceString {
     public static void main(String[] args) {
         FindIndexFirstOccurrenceString f = new FindIndexFirstOccurrenceString();
-        String haystack = "sadbutsad";
-        String needle = "sad";
+        String haystack = "mississippi";
+        String needle = "issipi";
+
         int indice = f.strStr(haystack, needle);
         System.out.println(indice);
+
+        int indice2 = f.solucao2(haystack, needle);
+        System.out.println(indice2);
     }
 
     public int strStr(String haystack, String needle) {
         return haystack.indexOf(needle);
+    }
+
+    public int solucao2(String haystack, String needle) {
+        boolean encontrou = false;
+        int indice = 0;
+        for(int i = 0; i < haystack.length(); i++) {
+            if(haystack.charAt(i) == needle.charAt(0)) {
+                encontrou = true;
+                indice = i;
+                for(int j = 1; j < needle.length(); j++) {
+                    if(j >= haystack.length() || j+i >= haystack.length()) {
+                        return -1;
+                    }
+                    if (!(haystack.charAt(i + j) == needle.charAt(j))) {
+                        encontrou = false;
+                        break;
+                    }
+                }
+                if(encontrou) {
+                    return indice;
+                }
+            }
+        }
+        return -1;
     }
 }
